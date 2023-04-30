2ae1a0d277d5f8b45bac2e6ee8ec0b5aa13cdf94
Соколов Глеб <g.sokolov@21vek.local>
2023-04-30 18:15:45 +0300

    ad preview slider

--- src/js/components/SliderPopup.tsx
+++ src/js/components/SliderPopup.tsx
@@ -36,41 +36,91 @@ function SliderPopup(props: ApartProps) {
 		}
 	};
 
+	const isOddLength = props.Apartaments[props.activeSlider].description.length % 2 === 0;
+	const oddCount =
+		props.Apartaments[props.activeSlider].description.length % 2 === 0
+			? props.Apartaments[props.activeSlider].description.length / 2
+			: (props.Apartaments[props.activeSlider].description.length - 1) / 2 + 1;
+
+	console.log(isOddLength, oddCount);
+
 	return (
 		<div
 			id="popup"
-			className={`relative w-[374rem] h-[377rem] lg:text-[20rem] text-white position z-[1000] lg:w-[1030rem] lg:h-[605rem] bg-cover bg-no-repeat overflow-hidden`}>
+			className={`relative w-[374rem] h-[377rem] lg:text-[20rem] text-white position z-[1000] lg:w-[1120rem] lg:h-[661rem] bg-cover bg-no-repeat overflow-hidden`}>
 			{!props.openSlider ? (
 				<div className="h-[100%] bg-white w-[100%] flex flex-row">
 					<div
 						className=" bg-cover w-[448rem] bg-[-200rem]"
 						style={{ backgroundImage: `url(${props.Apartaments[props.activeSlider].img})` }}></div>
 					<div className="lg:p-[48rem] w-[672rem]">
-						<div className="x">{props.Apartaments[props.activeSlider].name}</div>
-						<div className="x">
+						<div className="font-font1 font-[400] lg:text-[32rem] lg:leading-[31rem] text-[#1E1E1E] lg:mb-[32rem]">
+							{props.Apartaments[props.activeSlider].name}
+						</div>
+						<div className="font-font2 font-[400] text-[14rem] leading-[20rem] text-[#575757] mb-[18rem]">
 							Апартаменты идеально подойдут для командировок и туристического отдыха. Во дворе есть
 							детская площадка и бесплатная охраняемая парковка. Весь двор оборудован
 							видеонаблюдением. В здании дома находится фитнес-центр, салон красоты, офис такси,
 							охраняемая парковка.{' '}
 						</div>
-						<div className="x">Что мы предлагаем:</div>
-						<div className="x"></div>
-						<div className="x">
+						<div className="font-font2 font-[600] text-[14rem] leading-[19rem] text-[#1E1E1E] mb-[18rem]">
+							Что мы предлагаем:
+						</div>
+						<div
+							className={`grid grid-flow-col gap-[6rem]  
+						${isOddLength ? ` grid-rows-[${oddCount}] mb-[53rem]` : ` grid-rows-[${oddCount}] mb-[33rem]`}`}
+							style={{
+								gridTemplateRows: `repeat(${
+									isOddLength ? `${oddCount}` : `${oddCount}`
+								}, minmax(0, 1fr))`,
+							}}>
+							{props.Apartaments[props.activeSlider].description.map((description, index) => {
+								return (
+									<div key={index} className="flex flex-row gap-[12rem] items-center">
+										<div className="s">
+											<svg
+												width="12"
+												height="2"
+												viewBox="0 0 12 2"
+												fill="none"
+												xmlns="http://www.w3.org/2000/svg">
+												<path
+													fillRule="evenodd"
+													clipRule="evenodd"
+													d="M0 0.25L12 0.25V1.75L0 1.75V0.25Z"
+													fill="#D6AF85"
+												/>
+											</svg>
+										</div>
+										<div className="w-[200rem] h-[20rem] font-font2 font-[400] text-[14rem] leading-[20rem] text-[#575757]">
+											{description[0]}
+										</div>
+									</div>
+								);
+							})}
+						</div>
+						<div className="flex flex-row gap-[11rem] mb-[14rem]">
 							<div className="s">
 								<WiTime8 className="w-[30rem] h-[30rem] lg:w-[18rem] lg:h-[18rem] text-[#C8C3C3]" />
 							</div>
-							<div className="s">Заезд в любое время с 14:00. </div>
+							<div className="font-font2 font-[600] text-[16rem] leading-[22rem] text-[#B99772]">
+								Заезд в любое время с 14:00.{' '}
+							</div>
 						</div>
-						<div className="x">
+						<div className="flex flex-row gap-[11rem] mb-[49rem]">
 							<div className="s">
 								<IoLocationOutline className="w-[30rem] h-[30rem] lg:w-[18rem] lg:h-[18rem] text-[#C8C3C3]" />
 							</div>
-							<div className="s">ул. 17 сентября, 12</div>
+							<div className="font-font2 font-[600] text-[16rem] leading-[22rem] text-[#B99772]">
+								ул. 17 сентября, 12
+							</div>
 						</div>
-						<div
-							onClick={() => props.setOpenSlider(true)}
-							className="lg:w-[181rem] lg:h-[56rem] bg-[#D6AF85] flex justify-center items-center font-font2 font-[600] text-[14rem] leading-[19rem] text-[#FAFAFA]">
-							Смотреть фото
+						<div className="w-full flex justify-center">
+							<div
+								onClick={() => props.setOpenSlider(true)}
+								className="cursor-pointer lg:w-[181rem] lg:h-[56rem] bg-[#D6AF85] flex justify-center items-center font-font2 font-[600] text-[14rem] leading-[19rem] text-[#FAFAFA]">
+								Смотреть фото
+							</div>
 						</div>
 					</div>
 				</div>
