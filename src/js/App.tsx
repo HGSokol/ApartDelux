cd3a40d9e17e8ea96e40827b4f144a42e52dba05
Соколов Глеб <g.sokolov@21vek.local>
2023-05-03 13:40:06 +0300

    add lazy img

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -6,17 +6,6 @@ import ContactInfo from './layouts/ContactInfo';
 import Footer from './layouts/Footer';
 import SliderPopup from '../js/components/SliderPopup';
 
-import Bed1 from '../img/bed-1.svg';
-import Bed from '../img/bed.svg';
-import AirConditioner from '../img/air-conditioner.svg';
-import WashingMachine from '../img/washing-machine1.svg';
-import WiFi from '../img/laptop.svg';
-import Hairdryer from '../img/hairdryer.svg';
-import TV from '../img/tv1.svg';
-import Parking from '../img/parking.svg';
-import Towel from '../img/towel.svg';
-import FireExtinguisher from '../img/fire-extinguisher.svg';
-
 import img1 from '../img/rect1.png';
 import img2 from '../img/rect2.png';
 import img3 from '../img/rect3.png';
@@ -28,240 +17,239 @@ import img8 from '../img/rect8.png';
 import img9 from '../img/rect9.png';
 
 export interface Idata {
-  img: string;
-  name: string;
-  descriptions: [string, string][];
-  adress: string;
-  photos: string[];
+	img: string;
+	name: string;
+	descriptions: string[];
+	adress: string;
+	photos: string[];
 }
 
 export const Apartaments: Idata[] = [
-  {
-    img: img1,
-    name: 'Deluxe Apart 1',
-    descriptions: [
-      ['1 диван-кровать', Bed1],
-      ['1 двуспальная кровать', Bed],
-      ['Постельное белье', ''],
-      ['Стиральная машина', WashingMachine],
-      ['Кондиционер', AirConditioner],
-      ['Полотенца', Towel],
-      ['Фен', Hairdryer],
-      ['Парковка', Parking],
-      ['Телевизор', TV],
-      ['Бесплатный wi-fi', WiFi],
-      ['Туалетно-косметические пренадлежности', FireExtinguisher],
-    ],
-    adress: 'Адрес ул 17 сентября, 12',
-    photos: [img3, img2, img1, img4, img5, img6],
-  },
-  {
-    img: img2,
-    name: 'Deluxe Apart 2',
-    descriptions: [
-      ['1 диван-кровать', Bed1],
-      ['1 двуспальная кровать', Bed],
-      ['Постельное белье', ''],
-      ['Стиральная машина', WashingMachine],
-      ['Кондиционер', AirConditioner],
-      ['Полотенца', Towel],
-      ['Фен', Hairdryer],
-      ['Парковка', Parking],
-      ['Телевизор', TV],
-      ['Бесплатный wi-fi', WiFi],
-      ['Туалетно-косметические пренадлежности', FireExtinguisher],
-    ],
-    adress: 'Адрес ул 17 сентября, 12',
-    photos: [img3, img2, img1, img4, img5, img6],
-  },
-  {
-    img: img3,
-    name: 'Deluxe Apart 3',
-    descriptions: [
-      ['1 двуспальная кровать', Bed],
-      ['Бесплатный wi-fi', WiFi],
-      ['Постельное белье', ''],
-      ['Стиральная машина', WashingMachine],
-      ['Кондиционер', AirConditioner],
-      ['Полотенца', Towel],
-      ['Фен', Hairdryer],
-      ['Парковка', Parking],
-      ['Телевизор', TV],
-      ['Туалетно-косметические пренадлежности', FireExtinguisher],
-    ],
-    adress: 'Адрес ул 17 сентября, 12',
-    photos: [img3, img2, img1, img4, img5, img6],
-  },
-  {
-    img: img4,
-    name: 'Deluxe Apart 4',
-    descriptions: [
-      ['1 двуспальная кровать', Bed],
-      ['Бесплатный wi-fi', WiFi],
-      ['Постельное белье', ''],
-      ['Стиральная машина', WashingMachine],
-      ['Кондиционер', AirConditioner],
-      ['Полотенца', Towel],
-      ['Фен', Hairdryer],
-      ['Парковка', Parking],
-      ['Телевизор', TV],
-      ['Туалетно-косметические пренадлежности', FireExtinguisher],
-    ],
-    adress: 'Адрес ул 17 сентября, 12',
-    photos: [img3, img2, img1, img4, img5, img6],
-  },
-  {
-    img: img5,
-    name: 'Deluxe Apart 5',
-    descriptions: [
-      ['1 диван-кровать', Bed1],
-      ['1 двуспальная кровать', Bed],
-      ['Постельное белье', ''],
-      ['Стиральная машина', WashingMachine],
-      ['Кондиционер', AirConditioner],
-      ['Полотенца', Towel],
-      ['Фен', Hairdryer],
-      ['Парковка', Parking],
-      ['Телевизор', TV],
-      ['Бесплатный wi-fi', WiFi],
-      ['Туалетно-косметические пренадлежности', FireExtinguisher],
-    ],
-    adress: 'Адрес ул. Дзержинского, 34',
-    photos: [img3, img2, img1, img4, img5, img6],
-  },
-  {
-    img: img6,
-    name: 'Deluxe Apart 6',
-    descriptions: [
-      ['1 диван-кровать', Bed1],
-      ['1 двуспальная кровать', Bed],
-      ['Постельное белье', ''],
-      ['Стиральная машина', WashingMachine],
-      ['Бесплатный wi-fi', WiFi],
-      ['Полотенца', Towel],
-      ['Фен', Hairdryer],
-      ['Парковка', Parking],
-      ['Телевизор', TV],
-      ['Туалетно-косметические пренадлежности', FireExtinguisher],
-    ],
-    adress: 'Адрес ул 17 сентября, 12',
-    photos: [img3, img2, img1, img4, img5, img6],
-  },
-  {
-    img: img7,
-    name: 'Deluxe Apart 7',
-    descriptions: [
-      ['1 диван-кровать', Bed1],
-      ['1 двуспальная кровать', Bed],
-      ['Постельное белье', ''],
-      ['Стиральная машина', WashingMachine],
-      ['Бесплатный wi-fi', WiFi],
-      ['Полотенца', Towel],
-      ['Фен', Hairdryer],
-      ['Парковка', Parking],
-      ['Телевизор', TV],
-      ['Туалетно-косметические пренадлежности', FireExtinguisher],
-    ],
-    adress: 'Адрес ул 17 сентября, 12',
-    photos: [img3, img2, img1, img4, img5, img6],
-  },
-  {
-    img: img8,
-    name: 'Deluxe Apart 8',
-    descriptions: [
-      ['1 диван-кровать', Bed1],
-      ['1 двуспальная кровать', Bed],
-      ['Постельное белье', ''],
-      ['Стиральная машина', WashingMachine],
-      ['Кондиционер', AirConditioner],
-      ['Полотенца', Towel],
-      ['Фен', Hairdryer],
-      ['Парковка', Parking],
-      ['Телевизор', TV],
-      ['Бесплатный wi-fi', WiFi],
-      ['Туалетно-косметические пренадлежности', FireExtinguisher],
-    ],
-    adress: 'Адрес ул 17 сентября, 12',
-    photos: [img3, img2, img1, img4, img5, img6],
-  },
-  {
-    img: img9,
-    name: 'Deluxe Apart 9',
-    descriptions: [
-      ['1 диван-кровать', Bed1],
-      ['Бесплатный wi-fi', WiFi],
-      ['Постельное белье', ''],
-      ['Стиральная машина', WashingMachine],
-      ['Кондиционер', AirConditioner],
-      ['Полотенца', Towel],
-      ['Фен', Hairdryer],
-      ['Парковка', Parking],
-      ['Телевизор', TV],
-      ['Туалетно-косметические пренадлежности', FireExtinguisher],
-    ],
-    adress: 'Адрес ул 17 сентября, 12',
-    photos: [img3, img2, img1, img4, img5, img6],
-  },
+	{
+		img: img1,
+		name: 'Deluxe Apart 1',
+		descriptions: [
+			'1 диван-кровать',
+			'1 двуспальная кровать',
+			'Постельное белье',
+			'Стиральная машина',
+			'Кондиционер',
+			'Телевизор',
+			'Полотенца',
+			'Фен',
+			'Парковка',
+			'Бесплатный wi-fi',
+			'Туалетно-косметические пренадлежности',
+		],
+		adress: 'Адрес ул 17 сентября, 12',
+		photos: [img3, img2, img1, img4, img5, img6],
+	},
+	{
+		img: img2,
+		name: 'Deluxe Apart 2',
+		descriptions: [
+			'1 диван-кровать',
+			'1 двуспальная кровать',
+			'Постельное белье',
+			'Стиральная машина',
+			'Кондиционер',
+			'Телевизор',
+			'Полотенца',
+			'Фен',
+			'Парковка',
+			'Бесплатный wi-fi',
+			'Туалетно-косметические пренадлежности',
+		],
+		adress: 'Адрес ул 17 сентября, 12',
+		photos: [img3, img2, img1, img4, img5, img6],
+	},
+	{
+		img: img3,
+		name: 'Deluxe Apart 3',
+		descriptions: [
+			'1 двуспальная кровать',
+			'Постельное белье',
+			'Стиральная машина',
+			'Кондиционер',
+			'Телевизор',
+			'Фен',
+			'Полотенца',
+			'Парковка',
+			'Бесплатный wi-fi',
+			'Туалетно-косметические пренадлежности',
+		],
+		adress: 'Адрес ул 17 сентября, 12',
+		photos: [img3, img2, img1, img4, img5, img6],
+	},
+	{
+		img: img4,
+		name: 'Deluxe Apart 4',
+		descriptions: [
+			'1 двуспальная кровать',
+			'Постельное белье',
+			'Стиральная машина',
+			'Кондиционер',
+			'Телевизор',
+			'Фен',
+			'Полотенца',
+			'Парковка',
+			'Бесплатный wi-fi',
+			'Туалетно-косметические пренадлежности',
+		],
+		adress: 'Адрес ул 17 сентября, 12',
+		photos: [img3, img2, img1, img4, img5, img6],
+	},
+	{
+		img: img5,
+		name: 'Deluxe Apart 5',
+		descriptions: [
+			'1 диван-кровать',
+			'1 двуспальная кровать',
+			'Постельное белье',
+			'Стиральная машина',
+			'Кондиционер',
+			'Телевизор',
+			'Фен',
+			'Полотенца',
+			'Парковка',
+			'Бесплатный wi-fi',
+			'Туалетно-косметические пренадлежности',
+		],
+		adress: 'Адрес ул. Дзержинского, 34',
+		photos: [img3, img2, img1, img4, img5, img6],
+	},
+	{
+		img: img6,
+		name: 'Deluxe Apart 6',
+		descriptions: [
+			'1 диван-кровать',
+			'1 двуспальная кровать',
+			'Постельное белье',
+			'Стиральная машина',
+			'Телевизор',
+			'Фен',
+			'Полотенца',
+			'Парковка',
+			'Бесплатный wi-fi',
+			'Туалетно-косметические пренадлежности',
+		],
+		adress: 'Адрес ул 17 сентября, 12',
+		photos: [img3, img2, img1, img4, img5, img6],
+	},
+	{
+		img: img7,
+		name: 'Deluxe Apart 7',
+		descriptions: [
+			'1 диван-кровать',
+			'1 двуспальная кровать',
+			'Постельное белье',
+			'Стиральная машина',
+			'Телевизор',
+			'Фен',
+			'Полотенца',
+			'Парковка',
+			'Бесплатный wi-fi',
+			'Туалетно-косметические пренадлежности',
+		],
+		adress: 'Адрес ул 17 сентября, 12',
+		photos: [img3, img2, img1, img4, img5, img6],
+	},
+	{
+		img: img8,
+		name: 'Deluxe Apart 8',
+		descriptions: [
+			'1 диван-кровать',
+			'1 двуспальная кровать',
+			'Постельное белье',
+			'Стиральная машина',
+			'Кондиционер',
+			'Телевизор',
+			'Фен',
+			'Полотенца',
+			'Парковка',
+			'Бесплатный wi-fi',
+			'Туалетно-косметические пренадлежности',
+		],
+		adress: 'Адрес ул 17 сентября, 12',
+		photos: [img3, img2, img1, img4, img5, img6],
+	},
+	{
+		img: img9,
+		name: 'Deluxe Apart 9',
+		descriptions: [
+			'1 диван-кровать',
+			'Постельное белье',
+			'Стиральная машина',
+			'Кондиционер',
+			'Телевизор',
+			'Фен',
+			'Полотенца',
+			'Парковка',
+			'Бесплатный wi-fi',
+			'Туалетно-косметические пренадлежности',
+		],
+		adress: 'Адрес ул 17 сентября, 12',
+		photos: [img3, img2, img1, img4, img5, img6],
+	},
 ];
 
 function App() {
-  const [activePopup, setActivePopup] = useState(false);
-  const [openSlider, setOpenSlider] = useState(false);
-  const [activeSlider, setActiveSlider] = useState(0);
+	const [activePopup, setActivePopup] = useState(false);
+	const [openSlider, setOpenSlider] = useState(false);
+	const [activeSlider, setActiveSlider] = useState(0);
 
-  const closeSlider = (e) => {
-    const target = e.target as HTMLButtonElement;
+	const closeSlider = (e) => {
+		const target = e.target as HTMLButtonElement;
 
-    if (target.matches('#sliderCl')) {
-      setOpenSlider(false);
-      setActivePopup(false);
-    }
-  };
+		if (target.matches('#sliderCl')) {
+			setOpenSlider(false);
+			setActivePopup(false);
+		}
+	};
 
-  useEffect(() => {
-    document.addEventListener('click', closeSlider);
-    return () => {
-      document.removeEventListener('click', closeSlider);
-    };
-  }, []);
+	useEffect(() => {
+		document.addEventListener('click', closeSlider);
+		return () => {
+			document.removeEventListener('click', closeSlider);
+		};
+	}, []);
 
-  if (activePopup) {
-    document.body.classList.add('active-modal');
-  } else {
-    document.body.classList.remove('active-modal');
-  }
+	if (activePopup) {
+		document.body.classList.add('active-modal');
+	} else {
+		document.body.classList.remove('active-modal');
+	}
 
-  return (
-    <div className={`bg-[#1E1E1E] `}>
-      {activePopup ? (
-        <div className=" bg-slate-200 ">
-          <div
-            id="sliderCl"
-            className="h-[100vh] w-[100vw] grid items-center place-content-center xxx z-[1000] fixed bg-[#000000]/[0.8] backdrop-blur-[4px]"
-          >
-            <SliderPopup
-              // Apartaments={Apartaments}
-              setActivePopup={setActivePopup}
-              setOpenSlider={setOpenSlider}
-              openSlider={openSlider}
-              activeSlider={activeSlider}
-            />
-          </div>
-        </div>
-      ) : null}
-      <div className=" z-1  relative">
-        <Header />
-        <Preview />
-        <Galery
-          // Apartaments={Apartaments}
-          setActivePopup={setActivePopup}
-          setActiveSlider={setActiveSlider}
-        />
-        <ContactInfo />
-      </div>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className={`bg-[#1E1E1E] `}>
+			{activePopup ? (
+				<div className=" bg-slate-200 ">
+					<div
+						id="sliderCl"
+						className="h-[100vh] w-[100vw] grid items-center place-content-center xxx z-[1000] fixed bg-[#000000]/[0.8] backdrop-blur-[4px]">
+						<SliderPopup
+							// Apartaments={Apartaments}
+							setActivePopup={setActivePopup}
+							setOpenSlider={setOpenSlider}
+							openSlider={openSlider}
+							activeSlider={activeSlider}
+						/>
+					</div>
+				</div>
+			) : null}
+			<div className=" z-1  relative">
+				<Header />
+				<Preview />
+				<Galery
+					// Apartaments={Apartaments}
+					setActivePopup={setActivePopup}
+					setActiveSlider={setActiveSlider}
+				/>
+				<ContactInfo />
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- src/js/layouts/Header.tsx
+++ src/js/layouts/Header.tsx
@@ -1,6 +1,4 @@
-// import { useState, useEffect } from 'react';
-// import { RxCross2 } from 'react-icons/rx';
-// import { HiArrowLongRight } from 'react-icons/hi2';
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 import { RxDoubleArrowDown } from 'react-icons/rx';
 import { FiPhone } from 'react-icons/fi';
 import { CgInstagram } from 'react-icons/cg';
@@ -10,129 +8,128 @@ import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 import { Apartaments } from '../App';
 
 function Header() {
-  // const [openMenu, setOpenMenu] = useState(false);
+	// const [openMenu, setOpenMenu] = useState(false);
 
-  // const closeBurger = (e) => {
-  //   const target = e.target as HTMLButtonElement;
+	// const closeBurger = (e) => {
+	//   const target = e.target as HTMLButtonElement;
 
-  //   if (target.matches('#burgerCl')) {
-  //     setOpenMenu(false);
-  //   }
-  // };
+	//   if (target.matches('#burgerCl')) {
+	//     setOpenMenu(false);
+	//   }
+	// };
 
-  // useEffect(() => {
-  //   document.addEventListener('click', closeBurger);
-  //   return () => {
-  //     document.removeEventListener('click', closeBurger);
-  //   };
-  // }, []);
+	// useEffect(() => {
+	//   document.addEventListener('click', closeBurger);
+	//   return () => {
+	//     document.removeEventListener('click', closeBurger);
+	//   };
+	// }, []);
 
-  // if (openMenu) {
-  //   document.body.classList.add('active-burger');
-  // } else {
-  //   document.body.classList.remove('active-burger');
-  // }
+	// if (openMenu) {
+	//   document.body.classList.add('active-burger');
+	// } else {
+	//   document.body.classList.remove('active-burger');
+	// }
 
-  return (
-    <div className="mb-[80rem] lg:mb-[149rem] overflow-hidden">
-      <div className=" px-[16rem] pt-[24rem] bg-[length:760rem_640rem] bg-[-245rem] bg-no-repeat bg-[url('../img/header1_1.png')] lg:bg-none transition duration-[400ms] h-[640rem] lg:h-[900rem] lg:flex lg:flex-row mb-[36rem] lg:mb-[0rem] flex flex-row justify-between flex-wrap lg:flex-nowrap lg:justify-normal lg:pt-0 lg:pl-[41rem]">
-        <div className=" flex flex-row gap-[11rem] items-center h-[17rem] lg:order-3 lg:pr-[100rem] lg:pt-[38rem]">
-          <div className="">
-            <FiPhone className="w-[15rem] h-[15rem] text-[#C8C3C3]" />
-          </div>
-          <div className="font-font2 font-[400] text-[14rem] leading-[19rem] text-[#C8C3C3] ">
-            +375 (29) 221-00-00
-          </div>
-        </div>
-        <div className="w-max flex flex-col lg:items-center lg:pt-[40rem] lg:order-1 lg:mr-[41rem] lg:w-[40rem]">
-          <div className="hidden lg:flex lg:flex-col lg:gap-[27rem] lg:mb-[83rem]">
-            <div className="lg:relative mode lg:-rotate-180 lg:font-font1 lg:font-[300] lg:text-[16rem] lg:text-[#FFFFFF] lg:whitespace-nowrap">
-              <div className="">Deluxe Apart</div>
-            </div>
-          </div>
-          <div className="hidden lg:flex bg-[#D6AF85] w-[91rem] h-[1rem] -rotate-90 whitespace-nowrap lg:mb-[470rem]"></div>
-          <div className="flex flex-col gap-[24rem] lg:gap-[38rem]">
-            <div className="cursor-pointer">
-              <CgInstagram className="w-[19.5rem] h-[19.5rem] lg:w-[22rem] lg:h-[22rem] text-[#C8C3C3] hover:text-[#D6AF85]" />
-            </div>
-            <div className="cursor-pointer">
-              <TbBrandTelegram className="w-[19.5rem] h-[19.5rem] lg:w-[22rem] lg:h-[22rem] text-[#C8C3C3] hover:text-[#D6AF85]" />
-            </div>
-            <div className="cursor-pointer">
-              <RiFacebookBoxLine className="w-[19.5rem] h-[19.5rem] lg:w-[22rem] lg:h-[22rem] text-[#C8C3C3] hover:text-[#D6AF85]" />
-            </div>
-          </div>
-        </div>
-        <div className="lg:bg-[url('../img/header1_1.png')] lg:pt-[39rem] lg:pl-[85rem] lg:bg-contain lg:w-[890rem] lg:bg-no-repeat lg:order-2 lg:mr-[146rem] lg:relative">
-          <div className="hidden lg:flex lg:flex-row lg:gap-[29rem] lg:absolute lg:translate-x-[618rem] lg:translate-y-[590rem]">
-            <div
-              className="bg-cover bg-no-repeat bg-[-25rem] lg:w-[225rem] lg:h-[209rem]"
-              style={{
-                backgroundImage: `url(${Apartaments[1].img})`,
-              }}
-            ></div>
-            <div
-              className="bg-cover bg-no-repeat bg-[-30rem] lg:w-[225rem] lg:h-[209rem]"
-              style={{
-                backgroundImage: `url(${Apartaments[5].img})`,
-              }}
-            ></div>
-            <div
-              className="bg-cover bg-no-repeat bg-[-43rem] lg:w-[225rem] lg:h-[209rem]"
-              style={{
-                backgroundImage: `url(${Apartaments[0].img})`,
-              }}
-            ></div>
-          </div>
-          <div className="hidden lg:flex lg:flex-row lg:gap-[48rem] lg:font-font2 lg:font-[700rem] lg:text-[16rem] lg:leading-[22rem] lg:mb-[145rem]">
-            <a
-              href={'#about'}
-              className="duration-[400ms] cursor-pointer text-[#C8C3C3] hover:text-[#D6AF85]"
-            >
-              О нас
-            </a>
-            <a
-              href={'#galery'}
-              className="duration-[400ms] cursor-pointer text-[#C8C3C3] hover:text-[#D6AF85]"
-            >
-              Галерея
-            </a>
-            <a
-              href={'#contacts'}
-              className="duration-[400ms] cursor-pointer text-[#C8C3C3] hover:text-[#D6AF85]"
-            >
-              Контакты
-            </a>
-          </div>
-          <div className="">
-            <div className="font-font1 font-[700rem] text-[48rem] lg:text-[128rem] leading-[46rem] lg:leading-[123rem] lg:tracking-[2.1rem] text-[#FAFAFA] mb-[24rem] lg:mb-[49rem]">
-              Deluxe Apartments
-            </div>
-            <div className="w-[290rem] font-font2 font-[400rem] text-[14rem] lg:text-[16rem] leading-[19rem] lg:leading-[22rem] lg:w-[440rem] text-[#FAFAFA] mb-[48rem] lg:mb-[68rem]">
-              Апартаменты являются частью элитного квартирного комплекса и находятся в самом центре
-              города Бреста.
-            </div>
-            <a
-              href={'#contacts'}
-              className="group cursor-pointer duration-[500ms] hover:bg-[#B99772] lg:pl-[36rem] lg:hover:w-[232rem] w-full lg:w-[200rem] h-[56rem] bg-[#D6AF85] flex flex-row justify-center lg:justify-normal  items-center "
-            >
-              <div className="lg:mr-[18rem] font-font2 font-[600] text-[14rem] leading-[19rem] text-[#FAFAFA]">
-                Связаться с нами
-              </div>
-              {window.innerWidth >= 1024 ? (
-                <div className="duration-[500ms] opacity-0 lg:group-hover:opacity-100 font-font2 font-[600] text-[14rem] leading-[19rem] text-[#FAFAFA]">
-                  <HiOutlineArrowNarrowRight className="w-[19.5rem] h-[19.5rem] lg:w-[22rem] lg:h-[22rem]" />
-                </div>
-              ) : null}
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="lg:hidden w-full flex items-center justify-center">
-        <RxDoubleArrowDown className="w-[30rem] h-[30rem] text-[#C8C3C3]" />
-      </div>
-    </div>
-  );
+	return (
+		<div className="mb-[80rem] lg:mb-[149rem] overflow-hidden">
+			<div className=" px-[16rem] pt-[24rem] bg-[length:760rem_640rem] bg-[-245rem] bg-no-repeat bg-[url('../img/header1_1.png')] lg:bg-none transition duration-[400ms] h-[640rem] lg:h-[900rem] lg:flex lg:flex-row mb-[36rem] lg:mb-[0rem] flex flex-row justify-between flex-wrap lg:flex-nowrap lg:justify-normal lg:pt-0 lg:pl-[41rem]">
+				<div className=" flex flex-row gap-[11rem] items-center h-[17rem] lg:order-3 lg:pr-[100rem] lg:pt-[38rem]">
+					<div className="">
+						<FiPhone className="w-[15rem] h-[15rem] text-[#C8C3C3]" />
+					</div>
+					<div className="font-font2 font-[400] text-[14rem] leading-[19rem] text-[#C8C3C3] ">
+						+375 (29) 221-00-00
+					</div>
+				</div>
+				<div className="w-max flex flex-col lg:items-center lg:pt-[40rem] lg:order-1 lg:mr-[41rem] lg:w-[40rem]">
+					<div className="hidden lg:flex lg:flex-col lg:gap-[27rem] lg:mb-[83rem]">
+						<div className="lg:relative mode lg:-rotate-180 lg:font-font1 lg:font-[300] lg:text-[16rem] lg:text-[#FFFFFF] lg:whitespace-nowrap">
+							<div className="">Deluxe Apart</div>
+						</div>
+					</div>
+					<div className="hidden lg:flex bg-[#D6AF85] w-[91rem] h-[1rem] -rotate-90 whitespace-nowrap lg:mb-[470rem]"></div>
+					<div className="flex flex-col gap-[24rem] lg:gap-[38rem]">
+						<div className="cursor-pointer">
+							<CgInstagram className="w-[19.5rem] h-[19.5rem] lg:w-[22rem] lg:h-[22rem] text-[#C8C3C3] hover:text-[#D6AF85]" />
+						</div>
+						<div className="cursor-pointer">
+							<TbBrandTelegram className="w-[19.5rem] h-[19.5rem] lg:w-[22rem] lg:h-[22rem] text-[#C8C3C3] hover:text-[#D6AF85]" />
+						</div>
+						<div className="cursor-pointer">
+							<RiFacebookBoxLine className="w-[19.5rem] h-[19.5rem] lg:w-[22rem] lg:h-[22rem] text-[#C8C3C3] hover:text-[#D6AF85]" />
+						</div>
+					</div>
+				</div>
+				<div className="lg:bg-[url('../img/header1_1.png')] lg:pt-[39rem] lg:pl-[85rem] lg:bg-contain lg:w-[890rem] lg:bg-no-repeat lg:order-2 lg:mr-[146rem] lg:relative">
+					<div className="hidden lg:flex lg:flex-row lg:gap-[29rem] lg:absolute lg:translate-x-[618rem] lg:translate-y-[590rem]">
+						<LazyLoadComponent>
+							<div
+								className="bg-cover bg-no-repeat bg-[-25rem] lg:w-[225rem] lg:h-[209rem]"
+								style={{
+									backgroundImage: `url(${Apartaments[1].img})`,
+								}}></div>
+						</LazyLoadComponent>
+						<LazyLoadComponent>
+							<div
+								className="bg-cover bg-no-repeat bg-[-30rem] lg:w-[225rem] lg:h-[209rem]"
+								style={{
+									backgroundImage: `url(${Apartaments[5].img})`,
+								}}></div>
+						</LazyLoadComponent>
+						<LazyLoadComponent>
+							<div
+								className="bg-cover bg-no-repeat bg-[-43rem] lg:w-[225rem] lg:h-[209rem]"
+								style={{
+									backgroundImage: `url(${Apartaments[0].img})`,
+								}}></div>
+						</LazyLoadComponent>
+					</div>
+					<div className="hidden lg:flex lg:flex-row lg:gap-[48rem] lg:font-font2 lg:font-[700rem] lg:text-[16rem] lg:leading-[22rem] lg:mb-[145rem]">
+						<a
+							href={'#about'}
+							className="duration-[400ms] cursor-pointer text-[#C8C3C3] hover:text-[#D6AF85]">
+							О нас
+						</a>
+						<a
+							href={'#galery'}
+							className="duration-[400ms] cursor-pointer text-[#C8C3C3] hover:text-[#D6AF85]">
+							Галерея
+						</a>
+						<a
+							href={'#contacts'}
+							className="duration-[400ms] cursor-pointer text-[#C8C3C3] hover:text-[#D6AF85]">
+							Контакты
+						</a>
+					</div>
+					<div className="">
+						<div className="font-font1 font-[700rem] text-[48rem] lg:text-[128rem] leading-[46rem] lg:leading-[123rem] lg:tracking-[2.1rem] text-[#FAFAFA] mb-[24rem] lg:mb-[49rem]">
+							Deluxe Apartments
+						</div>
+						<div className="w-[290rem] font-font2 font-[400rem] text-[14rem] lg:text-[16rem] leading-[19rem] lg:leading-[22rem] lg:w-[440rem] text-[#FAFAFA] mb-[48rem] lg:mb-[68rem]">
+							Апартаменты являются частью элитного квартирного комплекса и находятся в самом центре
+							города Бреста.
+						</div>
+						<a
+							href={'#contacts'}
+							className="group cursor-pointer duration-[500ms] hover:bg-[#B99772] lg:pl-[36rem] lg:hover:w-[232rem] w-full lg:w-[200rem] h-[56rem] bg-[#D6AF85] flex flex-row justify-center lg:justify-normal  items-center ">
+							<div className="lg:mr-[18rem] font-font2 font-[600] text-[14rem] leading-[19rem] text-[#FAFAFA]">
+								Связаться с нами
+							</div>
+							{window.innerWidth >= 1024 ? (
+								<div className="duration-[500ms] opacity-0 lg:group-hover:opacity-100 font-font2 font-[600] text-[14rem] leading-[19rem] text-[#FAFAFA]">
+									<HiOutlineArrowNarrowRight className="w-[19.5rem] h-[19.5rem] lg:w-[22rem] lg:h-[22rem]" />
+								</div>
+							) : null}
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="lg:hidden w-full flex items-center justify-center">
+				<RxDoubleArrowDown className="w-[30rem] h-[30rem] text-[#C8C3C3]" />
+			</div>
+		</div>
+	);
 }
 
 export default Header;
