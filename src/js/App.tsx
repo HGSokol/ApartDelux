e1fc18e92ec5e92e5466947ef21c6999b7cbc2a9
Gleb <g.sokolov@21vek.by>
2023-05-04 23:42:26 +0300

    add img

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -6,272 +6,273 @@ import ContactInfo from './layouts/ContactInfo';
 import Footer from './layouts/Footer';
 import SliderPopup from '../js/components/SliderPopup';
 
-import img1_1 from '../img/Header1.png';
-import img1_2 from '../img/Header1.png';
-import img1_3 from '../img/Header1.png';
-import img1_4 from '../img/Header1.png';
-import img2_1 from '../img/rect2.png';
-import img2_2 from '../img/rect2.png';
-import img2_3 from '../img/rect2.png';
-import img2_4 from '../img/rect2.png';
-import img3_1 from '../img/rect3.png';
-import img3_2 from '../img/rect3.png';
-import img3_3 from '../img/rect3.png';
-import img3_4 from '../img/rect3.png';
-import img4_1 from '../img/rect4.png';
-import img4_2 from '../img/rect4.png';
-import img4_3 from '../img/rect4.png';
-import img4_4 from '../img/rect4.png';
-import img5_1 from '../img/rect5.png';
-import img5_2 from '../img/rect5.png';
-import img5_3 from '../img/rect5.png';
-import img5_4 from '../img/rect5.png';
-import img6_1 from '../img/rect6.png';
-import img6_2 from '../img/rect6.png';
-import img6_3 from '../img/rect6.png';
-import img6_4 from '../img/rect6.png';
-import img7_1 from '../img/rect7.png';
-import img7_2 from '../img/rect7.png';
-import img7_3 from '../img/rect7.png';
-import img7_4 from '../img/rect7.png';
-import img8_1 from '../img/rect8.png';
-import img8_2 from '../img/rect8.png';
-import img8_3 from '../img/rect8.png';
-import img8_4 from '../img/rect8.png';
-import img9_1 from '../img/rect9.png';
-import img9_2 from '../img/rect9.png';
-import img9_3 from '../img/rect9.png';
-import img9_4 from '../img/Header2.png';
+import img1_1 from '../img/../img/apart1_1.jpg';
+import img1_2 from '../img/../img/apart1_2.jpg';
+import img1_3 from '../img/../img/apart1_3.jpg';
+import img1_4 from '../img/../img/apart1_4.jpg';
+import img2_1 from '../img/../img/apart2_1.jpg';
+import img2_2 from '../img/../img/apart2_2.jpg';
+import img2_3 from '../img/../img/apart2_3.jpg';
+import img2_4 from '../img/../img/apart2_4.jpg';
+import img3_1 from '../img/../img/apart3_1.jpg';
+import img3_2 from '../img/../img/apart3_2.jpg';
+import img3_3 from '../img/../img/apart3_3.jpg';
+import img3_4 from '../img/../img/apart3_4.jpg';
+import img4_1 from '../img/../img/apart4_1.jpg';
+import img4_2 from '../img/../img/apart4_2.jpg';
+import img4_3 from '../img/../img/apart4_3.jpg';
+import img4_4 from '../img/../img/apart4_4.jpg';
+import img5_1 from '../img/../img/apart5_1.jpg';
+import img5_2 from '../img/../img/apart5_2.jpg';
+import img5_3 from '../img/../img/apart5_3.jpg';
+import img5_4 from '../img/../img/apart5_4.jpg';
+import img6_1 from '../img/../img/apart6_1.jpg';
+import img6_2 from '../img/../img/apart6_2.jpg';
+import img6_3 from '../img/../img/apart6_3.jpg';
+import img6_4 from '../img/../img/apart6_4.jpg';
+import img7_1 from '../img/../img/apart7_1.jpg';
+import img7_2 from '../img/../img/apart7_2.jpg';
+import img7_3 from '../img/../img/apart7_3.jpg';
+import img7_4 from '../img/../img/apart7_4.jpg';
+import img8_1 from '../img/../img/apart8_1.jpg';
+import img8_2 from '../img/../img/apart8_2.jpg';
+import img8_3 from '../img/../img/apart8_3.jpg';
+import img8_4 from '../img/../img/apart8_4.jpg';
+import img9_1 from '../img/../img/apart9_1.jpg';
+import img9_2 from '../img/../img/apart9_2.jpg';
+import img9_3 from '../img/../img/apart9_3.jpg';
+import img9_4 from '../img/../img/apart9_4.jpg';
 
 export interface Idata {
-	img: string;
-	name: string;
-	descriptions: string[];
-	adress: string;
-	photos: string[];
+  img: string;
+  name: string;
+  descriptions: string[];
+  adress: string;
+  photos: string[];
 }
 
 export const Apartaments: Idata[] = [
-	{
-		img: img1_1,
-		name: 'Deluxe Apart 1',
-		descriptions: [
-			'1 диван-кровать',
-			'1 двуспальная кровать',
-			'Постельное белье',
-			'Стиральная машина',
-			'Кондиционер',
-			'Телевизор',
-			'Полотенца',
-			'Фен',
-			'Парковка',
-			'Бесплатный wi-fi',
-			'Туалетно-косметические пренадлежности',
-		],
-		adress: 'Адрес ул 17 сентября, 12',
-		photos: [img1_1, img1_2, img1_3, img1_4],
-	},
-	{
-		img: img2_1,
-		name: 'Deluxe Apart 2',
-		descriptions: [
-			'1 диван-кровать',
-			'1 двуспальная кровать',
-			'Постельное белье',
-			'Стиральная машина',
-			'Кондиционер',
-			'Телевизор',
-			'Полотенца',
-			'Фен',
-			'Парковка',
-			'Бесплатный wi-fi',
-			'Туалетно-косметические пренадлежности',
-		],
-		adress: 'Адрес ул 17 сентября, 12',
-		photos: [img2_1, img2_2, img2_3, img2_4],
-	},
-	{
-		img: img3_1,
-		name: 'Deluxe Apart 3',
-		descriptions: [
-			'1 двуспальная кровать',
-			'Постельное белье',
-			'Стиральная машина',
-			'Кондиционер',
-			'Телевизор',
-			'Фен',
-			'Полотенца',
-			'Парковка',
-			'Бесплатный wi-fi',
-			'Туалетно-косметические пренадлежности',
-		],
-		adress: 'Адрес ул 17 сентября, 12',
-		photos: [img3_1, img3_2, img3_3, img3_4],
-	},
-	{
-		img: img4_1,
-		name: 'Deluxe Apart 4',
-		descriptions: [
-			'1 двуспальная кровать',
-			'Постельное белье',
-			'Стиральная машина',
-			'Кондиционер',
-			'Телевизор',
-			'Фен',
-			'Полотенца',
-			'Парковка',
-			'Бесплатный wi-fi',
-			'Туалетно-косметические пренадлежности',
-		],
-		adress: 'Адрес ул 17 сентября, 12',
-		photos: [img4_1, img4_2, img4_3, img4_4],
-	},
-	{
-		img: img5_1,
-		name: 'Deluxe Apart 5',
-		descriptions: [
-			'1 диван-кровать',
-			'1 двуспальная кровать',
-			'Постельное белье',
-			'Стиральная машина',
-			'Кондиционер',
-			'Телевизор',
-			'Фен',
-			'Полотенца',
-			'Парковка',
-			'Бесплатный wi-fi',
-			'Туалетно-косметические пренадлежности',
-		],
-		adress: 'Адрес ул. Дзержинского, 34',
-		photos: [img5_1, img5_2, img5_3, img5_4],
-	},
-	{
-		img: img6_1,
-		name: 'Deluxe Apart 6',
-		descriptions: [
-			'1 диван-кровать',
-			'1 двуспальная кровать',
-			'Постельное белье',
-			'Стиральная машина',
-			'Телевизор',
-			'Фен',
-			'Полотенца',
-			'Парковка',
-			'Бесплатный wi-fi',
-			'Туалетно-косметические пренадлежности',
-		],
-		adress: 'Адрес ул 17 сентября, 12',
-		photos: [img6_1, img6_2, img6_3, img6_4],
-	},
-	{
-		img: img7_1,
-		name: 'Deluxe Apart 7',
-		descriptions: [
-			'1 диван-кровать',
-			'1 двуспальная кровать',
-			'Постельное белье',
-			'Стиральная машина',
-			'Телевизор',
-			'Фен',
-			'Полотенца',
-			'Парковка',
-			'Бесплатный wi-fi',
-			'Туалетно-косметические пренадлежности',
-		],
-		adress: 'Адрес ул 17 сентября, 12',
-		photos: [img7_1, img7_2, img7_3, img7_4],
-	},
-	{
-		img: img8_1,
-		name: 'Deluxe Apart 8',
-		descriptions: [
-			'1 диван-кровать',
-			'1 двуспальная кровать',
-			'Постельное белье',
-			'Стиральная машина',
-			'Кондиционер',
-			'Телевизор',
-			'Фен',
-			'Полотенца',
-			'Парковка',
-			'Бесплатный wi-fi',
-			'Туалетно-косметические пренадлежности',
-		],
-		adress: 'Адрес ул 17 сентября, 12',
-		photos: [img8_1, img8_2, img8_3, img8_4],
-	},
-	{
-		img: img9_1,
-		name: 'Deluxe Apart 9',
-		descriptions: [
-			'1 диван-кровать',
-			'Постельное белье',
-			'Стиральная машина',
-			'Кондиционер',
-			'Телевизор',
-			'Фен',
-			'Полотенца',
-			'Парковка',
-			'Бесплатный wi-fi',
-			'Туалетно-косметические пренадлежности',
-		],
-		adress: 'Адрес ул 17 сентября, 12',
-		photos: [img9_1, img9_2, img9_3, img9_4],
-	},
+  {
+    img: img1_1,
+    name: 'Deluxe Apart 1',
+    descriptions: [
+      '1 диван-кровать',
+      '1 двуспальная кровать',
+      'Постельное белье',
+      'Стиральная машина',
+      'Кондиционер',
+      'Телевизор',
+      'Полотенца',
+      'Фен',
+      'Парковка',
+      'Бесплатный wi-fi',
+      'Туалетно-косметические пренадлежности',
+    ],
+    adress: 'Адрес ул 17 сентября, 12',
+    photos: [img1_1, img1_2, img1_3, img1_4],
+  },
+  {
+    img: img2_1,
+    name: 'Deluxe Apart 2',
+    descriptions: [
+      '1 диван-кровать',
+      '1 двуспальная кровать',
+      'Постельное белье',
+      'Стиральная машина',
+      'Кондиционер',
+      'Телевизор',
+      'Полотенца',
+      'Фен',
+      'Парковка',
+      'Бесплатный wi-fi',
+      'Туалетно-косметические пренадлежности',
+    ],
+    adress: 'Адрес ул 17 сентября, 12',
+    photos: [img2_1, img2_2, img2_3, img2_4],
+  },
+  {
+    img: img3_1,
+    name: 'Deluxe Apart 3',
+    descriptions: [
+      '1 двуспальная кровать',
+      'Постельное белье',
+      'Стиральная машина',
+      'Кондиционер',
+      'Телевизор',
+      'Фен',
+      'Полотенца',
+      'Парковка',
+      'Бесплатный wi-fi',
+      'Туалетно-косметические пренадлежности',
+    ],
+    adress: 'Адрес ул 17 сентября, 12',
+    photos: [img3_1, img3_2, img3_3, img3_4],
+  },
+  {
+    img: img4_1,
+    name: 'Deluxe Apart 4',
+    descriptions: [
+      '1 двуспальная кровать',
+      'Постельное белье',
+      'Стиральная машина',
+      'Кондиционер',
+      'Телевизор',
+      'Фен',
+      'Полотенца',
+      'Парковка',
+      'Бесплатный wi-fi',
+      'Туалетно-косметические пренадлежности',
+    ],
+    adress: 'Адрес ул 17 сентября, 12',
+    photos: [img4_1, img4_2, img4_3, img4_4],
+  },
+  {
+    img: img5_1,
+    name: 'Deluxe Apart 5',
+    descriptions: [
+      '1 диван-кровать',
+      '1 двуспальная кровать',
+      'Постельное белье',
+      'Стиральная машина',
+      'Кондиционер',
+      'Телевизор',
+      'Фен',
+      'Полотенца',
+      'Парковка',
+      'Бесплатный wi-fi',
+      'Туалетно-косметические пренадлежности',
+    ],
+    adress: 'Адрес ул. Дзержинского, 34',
+    photos: [img5_1, img5_2, img5_3, img5_4],
+  },
+  {
+    img: img6_1,
+    name: 'Deluxe Apart 6',
+    descriptions: [
+      '1 диван-кровать',
+      '1 двуспальная кровать',
+      'Постельное белье',
+      'Стиральная машина',
+      'Телевизор',
+      'Фен',
+      'Полотенца',
+      'Парковка',
+      'Бесплатный wi-fi',
+      'Туалетно-косметические пренадлежности',
+    ],
+    adress: 'Адрес ул 17 сентября, 12',
+    photos: [img6_1, img6_2, img6_3, img6_4],
+  },
+  {
+    img: img7_1,
+    name: 'Deluxe Apart 7',
+    descriptions: [
+      '1 диван-кровать',
+      '1 двуспальная кровать',
+      'Постельное белье',
+      'Стиральная машина',
+      'Телевизор',
+      'Фен',
+      'Полотенца',
+      'Парковка',
+      'Бесплатный wi-fi',
+      'Туалетно-косметические пренадлежности',
+    ],
+    adress: 'Адрес ул 17 сентября, 12',
+    photos: [img7_1, img7_2, img7_3, img7_4],
+  },
+  {
+    img: img8_1,
+    name: 'Deluxe Apart 8',
+    descriptions: [
+      '1 диван-кровать',
+      '1 двуспальная кровать',
+      'Постельное белье',
+      'Стиральная машина',
+      'Кондиционер',
+      'Телевизор',
+      'Фен',
+      'Полотенца',
+      'Парковка',
+      'Бесплатный wi-fi',
+      'Туалетно-косметические пренадлежности',
+    ],
+    adress: 'Адрес ул 17 сентября, 12',
+    photos: [img8_1, img8_2, img8_3, img8_4],
+  },
+  {
+    img: img9_1,
+    name: 'Deluxe Apart 9',
+    descriptions: [
+      '1 диван-кровать',
+      'Постельное белье',
+      'Стиральная машина',
+      'Кондиционер',
+      'Телевизор',
+      'Фен',
+      'Полотенца',
+      'Парковка',
+      'Бесплатный wi-fi',
+      'Туалетно-косметические пренадлежности',
+    ],
+    adress: 'Адрес ул 17 сентября, 12',
+    photos: [img9_1, img9_2, img9_3, img9_4],
+  },
 ];
 
 function App() {
-	const [activePopup, setActivePopup] = useState(false);
-	const [openSlider, setOpenSlider] = useState(false);
-	const [activeSlider, setActiveSlider] = useState(0);
+  const [activePopup, setActivePopup] = useState(false);
+  const [openSlider, setOpenSlider] = useState(false);
+  const [activeSlider, setActiveSlider] = useState(0);
 
-	const closeSlider = (e) => {
-		const target = e.target as HTMLButtonElement;
+  const closeSlider = (e) => {
+    const target = e.target as HTMLButtonElement;
 
-		if (target.matches('#sliderCl')) {
-			setOpenSlider(false);
-			setActivePopup(false);
-		}
-	};
+    if (target.matches('#sliderCl')) {
+      setOpenSlider(false);
+      setActivePopup(false);
+    }
+  };
 
-	useEffect(() => {
-		document.addEventListener('click', closeSlider);
-		return () => {
-			document.removeEventListener('click', closeSlider);
-		};
-	}, []);
+  useEffect(() => {
+    document.addEventListener('click', closeSlider);
+    return () => {
+      document.removeEventListener('click', closeSlider);
+    };
+  }, []);
 
-	if (activePopup) {
-		document.body.classList.add('active-modal');
-	} else {
-		document.body.classList.remove('active-modal');
-	}
+  if (activePopup) {
+    document.body.classList.add('active-modal');
+  } else {
+    document.body.classList.remove('active-modal');
+  }
 
-	return (
-		<div className="bg-[#1E1E1E]">
-			{activePopup ? (
-				<div className="bg-slate-200">
-					<div
-						id="sliderCl"
-						className="h-[100vh] w-[100vw] grid items-center place-content-center z-[1000] fixed bg-[#000000]/[0.8] backdrop-blur-[4px]">
-						<SliderPopup
-							setActivePopup={setActivePopup}
-							setOpenSlider={setOpenSlider}
-							openSlider={openSlider}
-							activeSlider={activeSlider}
-						/>
-					</div>
-				</div>
-			) : null}
-			<div className="z-1 relative">
-				<Header />
-				<Preview />
-				<Galery setActivePopup={setActivePopup} setActiveSlider={setActiveSlider} />
-				<ContactInfo />
-			</div>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="bg-[#1E1E1E]">
+      {activePopup ? (
+        <div className="bg-slate-200">
+          <div
+            id="sliderCl"
+            className="h-[100vh] w-[100vw] grid items-center place-content-center z-[1000] fixed bg-[#000000]/[0.8] backdrop-blur-[4px]"
+          >
+            <SliderPopup
+              setActivePopup={setActivePopup}
+              setOpenSlider={setOpenSlider}
+              openSlider={openSlider}
+              activeSlider={activeSlider}
+            />
+          </div>
+        </div>
+      ) : null}
+      <div className="z-1 relative">
+        <Header />
+        <Preview />
+        <Galery setActivePopup={setActivePopup} setActiveSlider={setActiveSlider} />
+        <ContactInfo />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/js/layouts/Galery.tsx
+++ src/js/layouts/Galery.tsx
@@ -3,77 +3,81 @@ import { LazyLoadComponent } from 'react-lazy-load-image-component';
 import { Apartaments } from '../App';
 
 interface ApartProps {
-	setActivePopup: Dispatch<SetStateAction<boolean>>;
-	setActiveSlider: Dispatch<SetStateAction<number>>;
+  setActivePopup: Dispatch<SetStateAction<boolean>>;
+  setActiveSlider: Dispatch<SetStateAction<number>>;
 }
 
 function Galery(props: ApartProps) {
-	const [currentSliderList, setCurrentSliderList] = useState<number>(0);
+  const [currentSliderList, setCurrentSliderList] = useState<number>(0);
 
-	const left = () => {
-		if (currentSliderList >= 1) {
-			setCurrentSliderList((prev) => prev - 1);
-		}
-	};
-	const right = () => {
-		if (currentSliderList <= Apartaments.length - 5) {
-			setCurrentSliderList((prev) => prev + 1);
-		}
-	};
+  const left = () => {
+    if (currentSliderList >= 1) {
+      setCurrentSliderList((prev) => prev - 1);
+    }
+  };
+  const right = () => {
+    if (currentSliderList <= Apartaments.length - 5) {
+      setCurrentSliderList((prev) => prev + 1);
+    }
+  };
 
-	return (
-		<div id="galery" className="relative mb-[120rem] lg:mb-[200rem]">
-			<div className="ml-[16rem] lg:ml-[202rem] font-font1 font-[700] text-[32rem] lg:text-[48rem] leading-[31rem] lg:leading-[46rem] text-[#FAFAFA] mb-[48rem] lg:mb-[13rem]">
-				Галерея
-			</div>
-			<div className="hidden lg:flex right-0 w-full justify-end pr-[101rem] mb-[37rem] gap-[28rem]">
-				<div onClick={() => left()} className="text-white text-[20rem] cursor-pointer">
-					<svg
-						className="w-[92rem] h-[16rem] stroke-[#575757] hover:stroke-[#B99772] duration-[300ms]"
-						viewBox="0 0 92 16"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg">
-						<path d="M92 8L0.999999 8.00001M0.999999 8.00001L12.5 15.5M0.999999 8.00001L12.5 0.500007" />
-					</svg>
-				</div>
-				<div onClick={() => right()} className="text-white text-[20rem] cursor-pointer">
-					<svg
-						className="w-[92rem] h-[16rem] stroke-[#D6AF85] hover:stroke-[#B99772] duration-[300ms]"
-						viewBox="0 0 92 16"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg">
-						<path d="M0 8L91 8M91 8L79.5 0.5M91 8L79.5 15.5" />
-					</svg>
-				</div>
-			</div>
-			<div className="overflow-x-scroll lg:overflow-hidden snap-proximity -wekbit-scrollbar:w-[0rem]">
-				<div
-					className="relative flex flex-row w-max gap-[16rem] lg:gap-0 cursor-pointer duration-[600ms]"
-					style={{
-						transform: `translateX(-${currentSliderList * 360}rem)`,
-					}}>
-					{Apartaments.map((apartament, index) => {
-						return (
-							<LazyLoadComponent key={index}>
-								<div
-									onClick={() => {
-										props.setActivePopup(true);
-										props.setActiveSlider(index);
-									}}
-									className="relative bg-[-110rem] lg:bg-[-230rem] font-font2 bg-cover bg-no-repeat font-[400] text-[16rem] leading-[20rem] snap-center text-[#FAFAFA] w-[255rem] h-[400rem] lg:w-[360rem] lg:h-[635rem] "
-									style={{ backgroundImage: `url(${apartament.img})` }}>
-									<div className="absolute w-[100%] h-[100%] bg-black/[0.4] duration-[500ms] hover:bg-black/[0]"></div>
-									<div className="pt-[19rem] pl-[21rem] lg:pl-[30rem] lg:pt-[30rem] w-[92rem] h-[35rem] lg:w-[100rem] lg:h-[40rem]">
-										{apartament.name}
-									</div>
-								</div>
-							</LazyLoadComponent>
-						);
-					})}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div id="galery" className="relative mb-[120rem] lg:mb-[200rem]">
+      <div className="ml-[16rem] lg:ml-[202rem] font-font1 font-[700] text-[32rem] lg:text-[48rem] leading-[31rem] lg:leading-[46rem] text-[#FAFAFA] mb-[48rem] lg:mb-[13rem]">
+        Галерея
+      </div>
+      <div className="hidden lg:flex right-0 w-full justify-end pr-[101rem] mb-[37rem] gap-[28rem]">
+        <div onClick={() => left()} className="text-white text-[20rem] cursor-pointer">
+          <svg
+            className="w-[92rem] h-[16rem] stroke-[#575757] hover:stroke-[#B99772] duration-[300ms]"
+            viewBox="0 0 92 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M92 8L0.999999 8.00001M0.999999 8.00001L12.5 15.5M0.999999 8.00001L12.5 0.500007" />
+          </svg>
+        </div>
+        <div onClick={() => right()} className="text-white text-[20rem] cursor-pointer">
+          <svg
+            className="w-[92rem] h-[16rem] stroke-[#D6AF85] hover:stroke-[#B99772] duration-[300ms]"
+            viewBox="0 0 92 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 8L91 8M91 8L79.5 0.5M91 8L79.5 15.5" />
+          </svg>
+        </div>
+      </div>
+      <div className="overflow-x-scroll lg:overflow-hidden snap-proximity -wekbit-scrollbar:w-[0rem]">
+        <div
+          className="relative flex flex-row w-max gap-[16rem] lg:gap-0 cursor-pointer duration-[600ms]"
+          style={{
+            transform: `translateX(-${currentSliderList * 360}rem)`,
+          }}
+        >
+          {Apartaments.map((apartament, index) => {
+            return (
+              <LazyLoadComponent key={index}>
+                <div
+                  onClick={() => {
+                    props.setActivePopup(true);
+                    props.setActiveSlider(index);
+                  }}
+                  className="relative bg-[-110rem] lg:bg-[-230rem] font-font2 bg-cover bg-no-repeat font-[400] text-[16rem] leading-[20rem] snap-center text-[#FAFAFA] w-[255rem] h-[400rem] lg:w-[360rem] lg:h-[635rem] "
+                  style={{ backgroundImage: `url(${apartament.img})` }}
+                >
+                  <div className="absolute w-[100%] h-[100%] bg-black/[0.4] duration-[500ms] hover:bg-black/[0]"></div>
+                  <div className="pt-[19rem] pl-[21rem] lg:pl-[30rem] lg:pt-[30rem] w-[92rem] h-[35rem] lg:w-[100rem] lg:h-[40rem]">
+                    {apartament.name}
+                  </div>
+                </div>
+              </LazyLoadComponent>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Galery;
