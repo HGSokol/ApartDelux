e84ada90fc8ec4f496f620e0efa106c7b254bcfc
Gleb <g.sokolov@21vek.by>
2023-09-15 21:08:51 +0300

    fix-text

--- src/js/layouts/Preview.tsx
+++ src/js/layouts/Preview.tsx
@@ -1,81 +1,79 @@
 function Preview() {
-	return (
-		<div id="about" className="lg:pt-[120rem] mx-[16rem] relative mb-[60rem] lg:mx-[125rem]">
-			<div className="absolute -translate-x-[70rem] lg:tracking-[3rem] lg:-translate-x-[224rem] -translate-y-[15rem] lg:-translate-y-[60rem] font-font1 font-[700] text-[64rem] lg:text-[128rem] leading-[61rem] lg:leading-[123rem] text-[#FAFAFA]/[0.1]">
-				О НАС
-			</div>
-			<div className="font-font1 font-[700] text-[32rem] lg:text-[48rem] leading-[31rem] lg:leading-[46rem] text-[#FAFAFA] mb-[53rem]">
-				О нас
-			</div>
-			<div className="flex flex-col lg:flex-row lg:gap-[115rem]">
-				<div className="flex flex-col">
-					<div className="flex flex-row justify-between mb-[48rem] lg:order-2 lg:gap-[85rem]">
-						<div className="flex flex-row gap-[28rem] justify-center items-center">
-							<div>
-								<svg
-									className="w-[50rem] h-[57rem] lg:w-[62rem] lg:h-[68rem]"
-									viewBox="0 0 52 57"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg">
-									<path
-										d="M0.800049 55.15C0.385835 55.15 0.0500488 55.4858 0.0500488 55.9C0.0500488 56.3142 0.385835 56.65 0.800049 56.65V55.15ZM50.3 56.65C50.7143 56.65 51.05 56.3142 51.05 55.9C51.05 55.4858 50.7143 55.15 50.3 55.15V56.65ZM30.9366 6.95914L30.7547 7.68675L30.9366 6.95914ZM10.882 1.17239C10.4801 1.07193 10.0729 1.31625 9.97244 1.7181C9.87198 2.11994 10.1163 2.52715 10.5181 2.62761L10.882 1.17239ZM30.9854 49.9681L31.1876 50.6903H31.1876L30.9854 49.9681ZM9.59783 55.1778C9.19895 55.2895 8.96614 55.7034 9.07783 56.1022C9.18951 56.5011 9.6034 56.7339 10.0023 56.6222L9.59783 55.1778ZM0.800049 56.65H8.00005V55.15H0.800049V56.65ZM10.55 54.1V2.8H9.05005V54.1H10.55ZM40.55 2.8V54.1H42.05V2.8H40.55ZM43.1 56.65H50.3V55.15H43.1V56.65ZM11.6 1.75H39.5V0.25H11.6V1.75ZM40.55 54.1C40.55 55.5083 41.6917 56.65 43.1 56.65V55.15C42.5201 55.15 42.05 54.6799 42.05 54.1H40.55ZM42.05 2.8C42.05 1.39167 40.9084 0.25 39.5 0.25V1.75C40.0799 1.75 40.55 2.2201 40.55 2.8H42.05ZM10.55 2.8C10.55 2.2201 11.0202 1.75 11.6 1.75V0.25C10.1917 0.25 9.05005 1.39168 9.05005 2.8H10.55ZM8.00005 56.65C9.40837 56.65 10.55 55.5083 10.55 54.1H9.05005C9.05005 54.6799 8.57995 55.15 8.00005 55.15V56.65ZM31.55 8.7054V48.2348H33.05V8.7054H31.55ZM31.1185 6.23153L10.882 1.17239L10.5181 2.62761L30.7547 7.68675L31.1185 6.23153ZM30.7832 49.2459L9.59783 55.1778L10.0023 56.6222L31.1876 50.6903L30.7832 49.2459ZM31.55 48.2348C31.55 48.7056 31.2366 49.1189 30.7832 49.2459L31.1876 50.6903C32.2888 50.382 33.05 49.3783 33.05 48.2348H31.55ZM33.05 8.7054C33.05 7.53528 32.2537 6.51533 31.1185 6.23153L30.7547 7.68675C31.2221 7.80361 31.55 8.22359 31.55 8.7054H33.05ZM27.05 29.8C27.05 29.8828 26.9829 29.95 26.9 29.95V31.45C27.8113 31.45 28.55 30.7113 28.55 29.8H27.05ZM26.9 29.95C26.8172 29.95 26.75 29.8828 26.75 29.8H25.25C25.25 30.7113 25.9888 31.45 26.9 31.45V29.95ZM26.75 29.8C26.75 29.7172 26.8172 29.65 26.9 29.65V28.15C25.9888 28.15 25.25 28.8887 25.25 29.8H26.75ZM26.9 29.65C26.9829 29.65 27.05 29.7172 27.05 29.8H28.55C28.55 28.8887 27.8113 28.15 26.9 28.15V29.65ZM26.15 28.9V30.7H27.65V28.9H26.15Z"
-										fill="#D6AF85"
-									/>
-								</svg>
-							</div>
-							<div>
-								<div className="font-font1 font-[700] text-[32rem] lg:text-[50rem] leading-[37rem] lg:leading-[59rem] text-[#FFFFFF] mb-[4rem] lg:mb-0">
-									9+
-								</div>
-								<div className="tracking-[0.06em] font-font2 font-[400] text-[14rem] lg:text-[16rem] lg:leading-[22rem] leading-[19rem] text-[#C4C4C4]">
-									Квартир
-								</div>
-							</div>
-						</div>
-						<div className="flex flex-row gap-[28rem] justify-center items-center">
-							<div>
-								<svg
-									className="w-[50rem] h-[57rem] lg:w-[62rem] lg:h-[68rem]"
-									viewBox="0 0 50 57"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg">
-									<path
-										d="M1 55C1 44.4206 8.82312 35.6684 19 34.2127M38.4928 40.6358C36.3935 38.2984 32.8927 37.6696 30.2624 39.81C27.6321 41.9504 27.2618 45.529 29.3274 48.0604C30.4936 49.4896 33.2643 51.995 35.4562 53.9059C36.4957 54.8122 37.0154 55.2654 37.6441 55.4508C38.1801 55.6089 38.8055 55.6089 39.3415 55.4508C39.9702 55.2654 40.49 54.8122 41.5295 53.9059C43.7213 51.995 46.492 49.4896 47.6582 48.0604C49.7238 45.529 49.3987 41.9279 46.7232 39.81C44.0477 37.6922 40.5921 38.2984 38.4928 40.6358ZM34 13C34 19.6274 28.6274 25 22 25C15.3726 25 10 19.6274 10 13C10 6.37258 15.3726 1 22 1C28.6274 1 34 6.37258 34 13Z"
-										stroke="#D6AF85"
-										strokeWidth="1.5"
-										strokeLinecap="round"
-										strokeLinejoin="round"
-									/>
-								</svg>
-							</div>
-							<div>
-								<div className="font-font1 font-[700] text-[32rem] lg:text-[50rem] leading-[37rem] lg:leading-[59rem] text-[#FFFFFF] mb-[4rem] lg:mb-0">
-									109+
-								</div>
-								<div className="tracking-[0.06em] font-font2 font-[400] text-[14rem] lg:text-[16rem] lg:leading-[22rem] leading-[19rem] text-[#C4C4C4]">
-									Клиентов
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="font-font1 font-[400] text-[24rem] lg:text-[40rem] leading-[23rem] lg:eading-[38rem] text-[#FFFFFF] mb-[32rem] lg:mb-[65rem] lg:order-1">
-						Deluxe Apart{' '}
-					</div>
-				</div>
-				<div className="tracking-[0.06em] w-[343rem] lg:w-[431rem] flex flex-col font-font2 font-[400] leading-[19rem] text-[14rem] lg:text-[16rem] lg:leading-[22rem] text-[#C8C3C3]">
-					<div className="mb-[16rem]">
-						Студия, в которой найдется квартира для Вас на любой вкус. Все апартаменты находятся в
-						самом сердце г. Бреста. Мы предлагаем Вам все удобства, необходимые для проживания как
-						на короткий, так и на длительный срок.
-					</div>
-					<div className="tracking-[0.9rem] w-[343rem] lg:w-[431rem]">
-						Мы предлагаем Вам все удобства, необходимые для проживания как на короткий, так и на
-						длительный срок.
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div id="about" className="lg:pt-[120rem] mx-[16rem] relative mb-[60rem] lg:mx-[125rem]">
+      <div className="absolute -translate-x-[70rem] lg:tracking-[3rem] lg:-translate-x-[224rem] -translate-y-[15rem] lg:-translate-y-[60rem] font-font1 font-[700] text-[64rem] lg:text-[128rem] leading-[61rem] lg:leading-[123rem] text-[#FAFAFA]/[0.1]">
+        О НАС
+      </div>
+      <div className="font-font1 font-[700] text-[32rem] lg:text-[48rem] leading-[31rem] lg:leading-[46rem] text-[#FAFAFA] mb-[53rem]">
+        О нас
+      </div>
+      <div className="flex flex-col lg:flex-row lg:gap-[115rem]">
+        <div className="flex flex-col">
+          <div className="flex flex-row justify-between mb-[48rem] lg:order-2 lg:gap-[85rem]">
+            <div className="flex flex-row gap-[28rem] justify-center items-center">
+              <div>
+                <svg
+                  className="w-[50rem] h-[57rem] lg:w-[62rem] lg:h-[68rem]"
+                  viewBox="0 0 52 57"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0.800049 55.15C0.385835 55.15 0.0500488 55.4858 0.0500488 55.9C0.0500488 56.3142 0.385835 56.65 0.800049 56.65V55.15ZM50.3 56.65C50.7143 56.65 51.05 56.3142 51.05 55.9C51.05 55.4858 50.7143 55.15 50.3 55.15V56.65ZM30.9366 6.95914L30.7547 7.68675L30.9366 6.95914ZM10.882 1.17239C10.4801 1.07193 10.0729 1.31625 9.97244 1.7181C9.87198 2.11994 10.1163 2.52715 10.5181 2.62761L10.882 1.17239ZM30.9854 49.9681L31.1876 50.6903H31.1876L30.9854 49.9681ZM9.59783 55.1778C9.19895 55.2895 8.96614 55.7034 9.07783 56.1022C9.18951 56.5011 9.6034 56.7339 10.0023 56.6222L9.59783 55.1778ZM0.800049 56.65H8.00005V55.15H0.800049V56.65ZM10.55 54.1V2.8H9.05005V54.1H10.55ZM40.55 2.8V54.1H42.05V2.8H40.55ZM43.1 56.65H50.3V55.15H43.1V56.65ZM11.6 1.75H39.5V0.25H11.6V1.75ZM40.55 54.1C40.55 55.5083 41.6917 56.65 43.1 56.65V55.15C42.5201 55.15 42.05 54.6799 42.05 54.1H40.55ZM42.05 2.8C42.05 1.39167 40.9084 0.25 39.5 0.25V1.75C40.0799 1.75 40.55 2.2201 40.55 2.8H42.05ZM10.55 2.8C10.55 2.2201 11.0202 1.75 11.6 1.75V0.25C10.1917 0.25 9.05005 1.39168 9.05005 2.8H10.55ZM8.00005 56.65C9.40837 56.65 10.55 55.5083 10.55 54.1H9.05005C9.05005 54.6799 8.57995 55.15 8.00005 55.15V56.65ZM31.55 8.7054V48.2348H33.05V8.7054H31.55ZM31.1185 6.23153L10.882 1.17239L10.5181 2.62761L30.7547 7.68675L31.1185 6.23153ZM30.7832 49.2459L9.59783 55.1778L10.0023 56.6222L31.1876 50.6903L30.7832 49.2459ZM31.55 48.2348C31.55 48.7056 31.2366 49.1189 30.7832 49.2459L31.1876 50.6903C32.2888 50.382 33.05 49.3783 33.05 48.2348H31.55ZM33.05 8.7054C33.05 7.53528 32.2537 6.51533 31.1185 6.23153L30.7547 7.68675C31.2221 7.80361 31.55 8.22359 31.55 8.7054H33.05ZM27.05 29.8C27.05 29.8828 26.9829 29.95 26.9 29.95V31.45C27.8113 31.45 28.55 30.7113 28.55 29.8H27.05ZM26.9 29.95C26.8172 29.95 26.75 29.8828 26.75 29.8H25.25C25.25 30.7113 25.9888 31.45 26.9 31.45V29.95ZM26.75 29.8C26.75 29.7172 26.8172 29.65 26.9 29.65V28.15C25.9888 28.15 25.25 28.8887 25.25 29.8H26.75ZM26.9 29.65C26.9829 29.65 27.05 29.7172 27.05 29.8H28.55C28.55 28.8887 27.8113 28.15 26.9 28.15V29.65ZM26.15 28.9V30.7H27.65V28.9H26.15Z"
+                    fill="#D6AF85"
+                  />
+                </svg>
+              </div>
+              <div>
+                <div className="font-font1 font-[700] text-[32rem] lg:text-[50rem] leading-[37rem] lg:leading-[59rem] text-[#FFFFFF] mb-[4rem] lg:mb-0">
+                  9+
+                </div>
+                <div className="tracking-[0.06em] font-font2 font-[400] text-[14rem] lg:text-[16rem] lg:leading-[22rem] leading-[19rem] text-[#C4C4C4]">
+                  Квартир
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row gap-[28rem] justify-center items-center">
+              <div>
+                <svg
+                  className="w-[50rem] h-[57rem] lg:w-[62rem] lg:h-[68rem]"
+                  viewBox="0 0 50 57"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 55C1 44.4206 8.82312 35.6684 19 34.2127M38.4928 40.6358C36.3935 38.2984 32.8927 37.6696 30.2624 39.81C27.6321 41.9504 27.2618 45.529 29.3274 48.0604C30.4936 49.4896 33.2643 51.995 35.4562 53.9059C36.4957 54.8122 37.0154 55.2654 37.6441 55.4508C38.1801 55.6089 38.8055 55.6089 39.3415 55.4508C39.9702 55.2654 40.49 54.8122 41.5295 53.9059C43.7213 51.995 46.492 49.4896 47.6582 48.0604C49.7238 45.529 49.3987 41.9279 46.7232 39.81C44.0477 37.6922 40.5921 38.2984 38.4928 40.6358ZM34 13C34 19.6274 28.6274 25 22 25C15.3726 25 10 19.6274 10 13C10 6.37258 15.3726 1 22 1C28.6274 1 34 6.37258 34 13Z"
+                    stroke="#D6AF85"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+              <div>
+                <div className="font-font1 font-[700] text-[32rem] lg:text-[50rem] leading-[37rem] lg:leading-[59rem] text-[#FFFFFF] mb-[4rem] lg:mb-0">
+                  109+
+                </div>
+                <div className="tracking-[0.06em] font-font2 font-[400] text-[14rem] lg:text-[16rem] lg:leading-[22rem] leading-[19rem] text-[#C4C4C4]">
+                  Клиентов
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="font-font1 font-[400] text-[24rem] lg:text-[40rem] leading-[23rem] lg:eading-[38rem] text-[#FFFFFF] mb-[32rem] lg:mb-[65rem] lg:order-1">
+            Deluxe Apart{' '}
+          </div>
+        </div>
+        <div className="tracking-[0.06em] w-[343rem] lg:w-[431rem] flex flex-col font-font2 font-[400] leading-[19rem] text-[14rem] lg:text-[16rem] lg:leading-[22rem] text-[#C8C3C3]">
+          <div className="mb-[16rem]">
+            Студия, в которой найдется квартира для Вас на любой вкус. Все апартаменты находятся в
+            самом сердце г. Бреста. Мы предлагаем Вам все удобства, необходимые для проживания как
+            на короткий, так и на длительный срок.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Preview;
